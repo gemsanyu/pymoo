a1a60b5d0812220fbdb85a7cfd8ddc41c9e9171f
gemilang <git.gemilang.santiyuda@gmail.com>
2020-11-29 09:56:22 +0700

    add tche2 and moead_cs

--- pymoo/decomposition/tchebicheff.py
+++ pymoo/decomposition/tchebicheff.py
@@ -9,3 +9,18 @@ class Tchebicheff(Decomposition):
         v = anp.abs(F - self.utopian_point) * weights
         tchebi = v.max(axis=1)
         return tchebi
+
+class Tchebicheff2(Decomposition):
+
+    def _do(self, F, weights, normalize=False, **kwargs):
+        w = weights
+        w[weights==0] = 10**6
+        f = F
+        if normalize:
+            if self.nadir_point is None:
+                raise("Tchebicheff2::_do: if normalize then nadir point must be provided")
+            f = (f-self.utopian_point)/(self.nadir_point-self.utopian_point)
+
+        v = anp.abs(f - self.utopian_point)/w
+        tchebi = v.max(axis=1)
+        return tchebi
