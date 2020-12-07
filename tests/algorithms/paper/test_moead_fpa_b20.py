5d39b4661fd27b8e537eaf48bc729b32b4bb0d33
gemilang <git.gemilang.santiyuda@gmail.com>
2020-12-07 08:48:00 +0700

    add 1 with 20 neighbors

---
+++ tests/algorithms/paper/test_moead_fpa_b20.py
@@ -0,0 +1,27 @@
+import sys
+
+from tests.test_alg import test_alg
+from pymoo.factory import get_reference_directions
+from pymoo.algorithms.moead_fpa_b import MOEAD_ALFPA_B
+from pymoo.decomposition.tchebicheff import Tchebicheff
+
+n_runs = int(sys.argv[1])
+problem_name = sys.argv[2]
+report_file_name = sys.argv[3]
+if len(sys.argv) > 4:
+    n_var = int(sys.argv[4])
+    n_obj = int(sys.argv[5])
+else:
+    n_obj = None
+    n_var = None
+
+print(n_runs, problem_name, report_file_name, n_var, n_obj)
+
+if n_obj is None:
+    ref_dirs = get_reference_directions("das-dennis", 2, n_partitions=99)
+else:
+    ref_dirs = get_reference_directions("das-dennis", n_obj, n_partitions=99)
+
+algorithm = MOEAD_ALFPA_B(ref_dirs, decomposition=Tchebicheff(), n_neighbors=20, n_replacement=2)
+
+test_alg(algorithm, n_runs, report_file_name, problem_name, n_var, n_obj)
